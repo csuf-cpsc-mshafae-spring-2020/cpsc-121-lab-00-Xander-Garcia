d3bc7ae1d581f4194cb05d292ab2a9443827095b
AlexGArcia98 <alexgarcia98@cs104u-2e.acad.fullerton.edu>
2020-01-21 15:48:59 -0800

    Need to get it to print out multiples from command line

--- part-2/hello_world.cpp
+++ part-2/hello_world.cpp
@@ -6,6 +6,37 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-  cout << "Hello world, this is cosmo!" << endl;
+
+  string word_entered = argv[1];
+  string amount_of_copies = argv[2];
+  int multiples = stoi(amount_of_copies);
+
+
+  if(argc > 4){
+    cout << "Too few arguments." << endl;
+    return 1;
+  }
+
+
+  do{
+
+    cout << "Hello " << word_entered << endl;
+
+  } while (multiples > multiples+1);
+
+
+  //Program he wrote on the white borad
+  // int main (int argc, char*argv[])
+  // cout << argc is << argc << endl;
+  // for( int i =0; i < argc; i++){
+    //cout << "i = " << i << " : " << arv[i] << endl;
+//}
+//return 0;
+//arg_demo.cpp
+//clang++ -----
+//./arg_dmeo A cat dog D
+
+
+
   return 0;
 }
