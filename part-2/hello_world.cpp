23dbc8f564a4155fd04d0a8de8c34a780c892f38
Alex <alexgarcia98@csu.fullerton.edu>
2020-01-26 20:22:39 -0800

    finished program

--- part-2/hello_world.cpp
+++ part-2/hello_world.cpp
@@ -6,37 +6,15 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-
+  if(argc != 3){
+    cout << "Not the correct amount of arguments." << endl;
+    return 0;
+  }
   string word_entered = argv[1];
   string amount_of_copies = argv[2];
   int multiples = stoi(amount_of_copies);
-
-
-  if(argc > 4){
-    cout << "Too few arguments." << endl;
-    return 1;
+  for(int i=0; i < multiples; i++){
+    cout << "Hello " << word_entered << "!" <<endl;
   }
-
-
-  do{
-
-    cout << "Hello " << word_entered << endl;
-
-  } while (multiples > multiples+1);
-
-
-  //Program he wrote on the white borad
-  // int main (int argc, char*argv[])
-  // cout << argc is << argc << endl;
-  // for( int i =0; i < argc; i++){
-    //cout << "i = " << i << " : " << arv[i] << endl;
-//}
-//return 0;
-//arg_demo.cpp
-//clang++ -----
-//./arg_dmeo A cat dog D
-
-
-
   return 0;
 }
